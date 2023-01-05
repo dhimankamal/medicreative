0bf836861ce93a6e066ed9b3a386ca0022bf96aa
kamal <dhimank079@gmail.com>
2023-01-05 08:24:43 +0530

    update seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,25 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import HeroSection from "components/HeroSection";
 import Services from "components/Services";
 import Testimonial from "components/Testimonial";
-import { useEffect } from "react";
 import ContactUs from "components/ContactUs";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-
   return (
     <>
       <Head>
         <title>Medicreative | Home</title>
-        <meta name="description" content="Medicreatives is a full-service digital agency that specializes in creating and maintaining websites for medical professionals."/>
+        <meta
+          name="description"
+          content="Medicreatives is a full-service digital agency that specializes in creating and maintaining websites for medical professionals."
+        />
+        <meta property="og:title" content="Medicreative | Home" />
+        <meta
+          property="og:description"
+          content="Medicreatives is a full-service digital agency that specializes in creating and maintaining websites for medical professionals."
+        />
+        <meta property="og:image" content="/assets/logo-white.svg" />
+        <meta property="og:url" content="https://www.medicreatives.net/"></meta>
       </Head>
       <main className="pb-16">
         <HeroSection />
